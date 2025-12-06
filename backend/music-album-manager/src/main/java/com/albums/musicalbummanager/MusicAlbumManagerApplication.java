fc4dbbe6787694240c34257ccb6e096e405d0b53
Roxana Daraban <roxana.daraban@student.upt.ro>
2025-12-06 16:04:31 +0200

    setting up frontend and backend

---
+++ backend/music-album-manager/src/main/java/com/albums/musicalbummanager/MusicAlbumManagerApplication.java
@@ -0,0 +1,13 @@
+package com.albums.musicalbummanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAlbumManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAlbumManagerApplication.class, args);
+	}
+
+}
---
+++ backend/music-album-manager/src/test/java/com/albums/musicalbummanager/MusicAlbumManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albums.musicalbummanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAlbumManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
